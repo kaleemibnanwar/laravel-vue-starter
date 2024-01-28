e00054d7c68e47a342c9faee03c66ae2472366f3
Kaleem Ibn Anwar <kaleemibnanwar@gmail.com>
2024-01-28 21:04:47 +0500

    changes for resume

---
+++ app/Http/Services/ThirdPartyServices/ResumeParser.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Http\Services\ThirdPartyServices;
+
+class ResumeParser
+{
+
+    const BASE_URL = '';
+    const API_KEY = '';
+    public $client = null;
+
+    public function __construct()
+    {
+
+    }
+
+    private function setupClient()
+    {
+
+    }
+
+    public function parseResume()
+    {
+
+    }
+}
